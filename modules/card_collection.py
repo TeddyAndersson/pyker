a99da4d187ef9f70c5d56b8aafd2cbd349e6947e
Teddy Andersson <teddyandersson95@gmail.com>
2021-04-25 20:11:18 +0200

    feat(card_collection): adding replace card method and forcing size param in constructor

--- modules/card_collection.py
+++ modules/card_collection.py
@@ -5,11 +5,15 @@ from .card_int import CardInt
 
 class CardCollection(list):
 
-    def __init__(self, evaluator: HandEvaluator = None):
+    def __init__(self, size: int = 2, evaluator: HandEvaluator = None):
+        super().__init__(None for i in range(size))
         self.rank = None
         self.strength = None
         self.potential = None
         self.evaluator = evaluator
+        print('collection', size)
+        if not size == None:
+            self = [None] * size
 
     def __str__(self):
         cards_string = ', '.join([str(card) for card in self])
@@ -22,6 +26,19 @@ class CardCollection(list):
         if not isinstance(item, CardInt):
             raise TypeError(f'{item} is not of instance {CardInt}')
         super(CardCollection, self).append(item)
+
+    def replace(self, item, index):
+        print('replace', len(self), index)
+        if not isinstance(item, CardInt):
+            raise TypeError(f'{item} is not of instance {CardInt}')
+
+        if not isinstance(index, int):
+            raise TypeError(f'{item} is not of instance {int}')
+
+        if index > len(self) - 1:
+            raise IndexError
+
+        self[index] = item
 
     def set_strength(self):
         pass
